d3f7f36a0aab25381c5ee579c565dbe3edc05175
vitosotdihaet <vitostikitos@gmail.com>
2024-05-27 00:09:14 +0300

    Add a main script

---
+++ main.py
@@ -0,0 +1,45 @@
+from metrics_collector import MetricsCollector
+
+from events.kafka_event import *
+from events.event import *
+
+import os
+
+# put the value in a k8s manifest
+kafka_bootstrap_server = os.environ.get('KAFKA_BOOTSTRAP_SERVER')
+prometheus_url = os.environ.get('PROMETHEUS_URL')
+redis_host = os.environ.get('REDIS_HOST')
+redis_port = os.environ.get('REDIS_PORT')
+
+
+event_queue = Queue()
+
+# share the queue with a reader
+reader = KafkaEventReader(
+    KafkaConsumer(
+        'mc',
+        bootstrap_servers=kafka_bootstrap_server
+    ),
+    event_queue
+)
+
+# writers to other microservices
+writers = {
+    'om': KafkaEventWriter(
+        KafkaProducer(
+            bootstrap_servers=kafka_bootstrap_server
+        ),
+        'om'
+    ),
+}
+
+# init the microservice
+metrics_collector = MetricsCollector(
+    event_queue, writers, prometheus_url,
+    Redis(
+        host=redis_host,
+        port=redis_port,
+    )
+)
+
+metrics_collector.main_thread.join()
